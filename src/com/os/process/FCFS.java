38dd124140bddabdcf8231dc027ad86f1e8e0cff
Samuel Yuan <yuans@rpi.edu>
2015-03-09 15:08:09 -0400

    Recalculate process statistics
    Calculate total turnaround and wait times after the program executes,
    not during execution,

--- src/com/os/process/FCFS.java
+++ src/com/os/process/FCFS.java
@@ -37,6 +37,32 @@ public class FCFS
 		return isCPUProcessDone;
 	}
 	
+	public static void printAllProcessStats(Process[] processes)
+	{
+		int minTurnaround = Integer.MAX_VALUE, maxTurnaround = Integer.MIN_VALUE; 
+		int minWait = Integer.MAX_VALUE, maxWait = Integer.MIN_VALUE;
+		double averageTurnaround = 0, averageWait = 0;
+		for (int i = 0; i < processes.length; i++)
+		{
+			//keep track of statistics
+			int processTurnaround = processes[i].getTotalTurnaround();
+			minTurnaround = Math.min(minTurnaround, processTurnaround);
+			maxTurnaround = Math.max(maxTurnaround, processTurnaround);
+			averageTurnaround += processTurnaround;
+			
+			int processWait = processes[i].getTotalWait();
+			minWait = Math.min(minWait, processWait);
+			maxWait = Math.max(maxWait, processWait);
+			averageWait += processWait;
+		}
+		
+		averageTurnaround /= (double)processes.length;
+		averageWait /= (double)processes.length;
+		
+		System.out.println("Turnaround time: min " + minTurnaround + "ms; avg " + String.format("%.2f", averageTurnaround) + "ms; max " + maxTurnaround + "ms");
+		System.out.println("Total wait time: min " + minWait + "ms; avg " + String.format("%.2f", averageWait) + "ms; max " + maxWait + "ms");
+	}
+	
 	public static void main(String[] args) 
 	{
 		//Simulation Parameters
@@ -47,52 +73,46 @@ public class FCFS
 		Process[] processes = new Process[12]; //assume 12 processes
 		initProcess(processes, probabilityInteractive);
 		
-		int overallTime = 0;
-		int minTurnaround = Integer.MAX_VALUE, maxTurnaround = Integer.MIN_VALUE;
-		int minWait = Integer.MAX_VALUE, maxWait = Integer.MIN_VALUE;
+		int currentTime = 0;
 		
 		//run all interactive processes
 		while (true)
 		{
 			for (int i = 0; i < processes.length; i++)
 			{
-				int beforeRan = overallTime;
+				int beforeRan = currentTime;
 				
 				//run the burst
-				overallTime += processes[i].getBurstTime();
+				currentTime += processes[i].getBurstTime();
 				int waitTime = processes[i].getWaitTime(beforeRan);
 				int turnaroundTime = processes[i].getWaitTime(beforeRan) + processes[i].getBurstTime();
-				System.out.println("[time " + overallTime + "ms] " + processes[i].getTypeString() + " process ID " + i + " CPU burst done " +
+				System.out.println("[time " + currentTime + "ms] " + processes[i].getTypeString() + " process ID " + i + " CPU burst done " +
 						 "(turnaround time " + turnaroundTime + "ms, total wait time " + waitTime + "ms)");
 				
-				//keep track of statistics
-				minTurnaround = Math.min(minTurnaround, turnaroundTime);
-				maxTurnaround = Math.max(maxTurnaround, turnaroundTime);
-				minWait = Math.min(minWait, waitTime);
-				maxWait = Math.max(maxWait, waitTime);
-		
 				processes[i].incrementTurnaround(turnaroundTime);
+				processes[i].incrementWait(waitTime);
+				
 				//adjust stats for cpu-bound processes only
 				if (processes[i].getType() == Process.TYPE_CPU)
 				{	
 					processes[i].decrementBursts();
 					if (processes[i].isBurstsDone())
 					{
-						System.out.println("[time " + overallTime + "ms] CPU-bound process ID " + i + " terminated " + 
+						System.out.println("[time " + currentTime + "ms] CPU-bound process ID " + i + " terminated " + 
 								"(total turnaround time " + processes[i].getTotalTurnaround() + "ms, total wait time xxxms");
 					}
 				}
 				
 				//human input delay (for interactive process)
-				overallTime += processes[i].getHumanResponseTime();
+				currentTime += processes[i].getHumanResponseTime();
 				
 				//put in the ready queue
-				processes[i].setLastTimeRan(overallTime);
+				processes[i].setLastTimeRan(currentTime);
 				
 				//context switch
-				System.out.println("[time " + overallTime + "ms] Context switch (swapping out process ID " + i +
+				System.out.println("[time " + currentTime + "ms] Context switch (swapping out process ID " + i +
 						" for process ID " + ((i + 1)%processes.length) + ")");
-				overallTime += timeContextSwitch;
+				currentTime += timeContextSwitch;
 			}
 
 			//terminate the program
@@ -100,8 +120,7 @@ public class FCFS
 				break;
 		}
 		
-		System.out.println("Turnaround time: min " + minTurnaround + " ms; avg ___ ms; max " + maxTurnaround + " ms");
-		System.out.println("Total wait time: min " + minWait + " ms; avg ___ ms; max " + maxWait + " ms");
+		printAllProcessStats(processes);
 	}
 
 }
--- src/com/os/process/Process.java
+++ src/com/os/process/Process.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Process 
 {
+	//A process is either interactive or cpu-bound
 	private int type;
 	public static final int TYPE_INTERACTIVE = 0;
 	public static final int TYPE_CPU = 1;
@@ -15,15 +16,7 @@ public class Process
 		return ""; 
 	}
 	
-	private int burstTime;
-	
-	//for interactive process
-	private int lastTimeRan;
-	
-	//for cpu-bound process
-	private int burstsLeft;
-	private int totalTurnaround;
-	
+	//random number generator for bursts
 	private Random randomObj;
 	
 	public Process(int type)
@@ -44,17 +37,32 @@ public class Process
 		}
 		
 		totalTurnaround = 0;
+		totalWait = 0;
 		lastTimeRan = 0;
 	}
 	
+	//each process has its own burst time
+	private int burstTime;
+	public int getBurstTime() { return burstTime; } 
+	
+	//between each process, there is a random block of time where the human spends time responding
 	public int getHumanResponseTime() { return randomObj.nextInt(4500 - 1000) + 1000; }
 	
-	public int getBurstTime() { return burstTime; } 
+	//for cpu-bound processes, there are a limited number of bursts.
+	//once the bursts run out, the process terminates
+	private int burstsLeft;
 	public void decrementBursts() { burstsLeft--; } 
 	public boolean isBurstsDone() { return (burstsLeft <= 0); } 
 		
+	//current wait time
+	private int lastTimeRan;
 	public int getWaitTime(int currentTime) { return currentTime - this.lastTimeRan; }
 	public void setLastTimeRan(int time) { this.lastTimeRan = time; }
+	
+	//process statistics (total wait time, total turnaround time)
+	private int totalTurnaround, totalWait;
+	public int getTotalWait() { return this.totalWait; }
+	public void incrementWait(int time) { this.totalWait += time; }
 	public int getTotalTurnaround() { return this.totalTurnaround; }
 	public void incrementTurnaround(int time) { this.totalTurnaround += time; }
 }
